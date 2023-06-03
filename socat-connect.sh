d734d8b0001097e87e4786b4d275a6a94c759248
Jeremiah K <17190268+jeremiah-k@users.noreply.github.com>
2023-06-03 17:16:02 -0500

    Update socat-connect.sh

--- socat-connect.sh
+++ socat-connect.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo socat pty,link=/dev/ttyVUSB0,raw,group-late=dialout,mode=660 tcp:192.168.100.10:8888
+sudo socat pty,link=/dev/ttyVUSB0,raw,group-late=dialout,mode=660 tcp:<yourhostsIP>:8888
